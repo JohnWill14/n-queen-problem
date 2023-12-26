5dd2c53f5f639559e6f38853fc962ae5bdbd0c1a
John William Vicente <johnwill.v.2017@gmail.com>
2023-12-25 23:38:55 -0300

    first CM

---
+++ Minimum Conflicts/NQueenMinimumConflicts/src/main/java/br/com/uem/informatica/ia/model/Board.java
@@ -0,0 +1,29 @@
+package br.com.uem.nformatica.ia.model;
+
+public class Board {
+    private byte matriz[][];
+    private int length;
+
+    public Board(int length) {
+        this.length = length;
+        matriz = new byte[length][length];
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public byte get(int row, int col){
+        return matriz[row][col];
+    }
+
+    public boolean update(byte value, int row, int col){
+        matriz[row][col] = value;
+        return true;
+    }
+
+}
---
+++ Minimum Conflicts/NQueenMinimumConflicts/src/main/java/br/com/uem/informatica/ia/solver/SolverNQ.java
@@ -0,0 +1,46 @@
+package br.com.uem.nformatica.ia.solver;
+
+import br.com.uem.informatica.ia.model.BoardNQueenProblem;
+import br.com.uem.informatica.ia.util.IOUtil;
+import br.com.uem.nformatica.ia.model.BoardNQueenProblem;
+
+public class SolverNQ {
+    private BoardNQueenProblem boardNQueenProblem;
+
+    public SolverNQ(int n) {
+        this.boardNQueenProblem = new BoardNQueenProblem(n);
+    }
+
+    public boolean solveNQ(){
+
+        if (!solve(0)) {
+            System.out.print("Solution does not exist");
+            return false;
+        }
+
+        return true;
+    }
+
+    private  boolean solve(int col){
+        // caso para parada
+        if (col >= boardNQueenProblem.getLength())
+            return true;
+
+        for (int i = 0; i < boardNQueenProblem.getLength(); i++) {
+
+            if (boardNQueenProblem.update((byte) 1, i, col)) {
+
+                if (solve(col + 1))
+                    return true;
+
+                boardNQueenProblem.update((byte) 0, i, col); // BACKTRACK
+            }
+        }
+
+        return false;
+    }
+
+    public void printBoard(){
+        IOUtil.printSolutionJSON(this.boardNQueenProblem);
+    }
+}
